0d58fc54a51ca275fb76d177041e11d55642d8ef
daniel-code20 <dgithub23@gmail.com>
2024-08-04 17:38:13 -0600

    update

--- keystone.ts
+++ keystone.ts
@@ -5,6 +5,8 @@
 // Keystone imports the default export of this file, expecting a Keystone configuration object
 //   you can find out more at https://keystonejs.com/docs/apis/config
 
+import 'dotenv/config'
+
 import { config } from '@keystone-6/core';
 
 // to keep this file tidy, we define our schema in a different file
@@ -37,9 +39,9 @@ export default withAuth(
       //   for more information on what database might be appropriate for you
       //   see https://keystonejs.com/docs/guides/choosing-a-database#title
       provider: 'postgresql',
-      url: process.env.DATABASE_URL ||  'DATABASE_URL_TO_REPLACE',
+      url: process.env.DATABASE_URL ||  'DATABASE_URL_TO_REPLACE' "
       
-      
+      onConnect: async context => { /* ... */ },
     },
     lists,
     session,
--- libra-lite-frontend/src/pages/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './BookDetail'
-export * from './BuyPage'
-export * from './CategoryDetail'
-export * from './Login'
-export * from './SignUp'
-export * from './PrincipalPage'
-export * from './ReservationPage'
