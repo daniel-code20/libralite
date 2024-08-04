e71b6ab34705a3a1509cb77300de0032326f71fc
Daniel Tejada <160541350+daniel-code20@users.noreply.github.com>
2024-08-04 15:24:41 -0600

    Update keystone.ts

--- my-app-libra-lite/keystone.ts
+++ my-app-libra-lite/keystone.ts
@@ -38,9 +38,8 @@ export default withAuth(
       // we're using sqlite for the fastest startup experience
       //   for more information on what database might be appropriate for you
       //   see https://keystonejs.com/docs/guides/choosing-a-database#title
-      provider: 'postgresql',
-      url: 'postgresql://postgres:VCqffUKJgppiQhUPTVeHsEQrviqzxtlA@postgres.railway.internal:5432/railway',
-      onConnect: async context => { /* ... */ },
+      provider: 'sqlite',
+      url: 'file:./keystone.db',
     },
     lists,
     session,
