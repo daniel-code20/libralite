7dc62b167d7da7ee5ad0a86d1085ec372e02563e
daniel-code20 <dgithub23@gmail.com>
2024-08-06 12:20:21 -0600

    upload

--- keystone.ts
+++ keystone.ts
@@ -3,10 +3,10 @@ import { config } from '@keystone-6/core';
 import { lists } from './schema';
 import { withAuth, session } from './auth';
 
-const S3_BUCKET_NAME=process.env.S3_BUCKET_NAME || 'S3_BUCKET_NAME'
-const S3_REGION=process.env.S3_REGION || 'S3_REGION'
-const S3_ACCESS_KEY_ID=process.env.S3_ACCESS_KEY_ID || 'S3_ACCESS_KEY_ID'
-const S3_SECRET_ACCESS_KEY=process.env.S3_SECRET_ACCESS_KEY|| 'S3_SECRET_ACCESS_KEY'
+const S3_BUCKET_NAME=process.env.S3_BUCKET_NAME || 'S3_BUCKET_NAME_TO_REPLACE'
+const S3_REGION=process.env.S3_REGION || 'S3_REGION_TO_REPLACE'
+const S3_ACCESS_KEY_ID=process.env.S3_ACCESS_KEY_ID || 'S3_ACCESS_KEY_ID_TO_REPLACE'
+const S3_SECRET_ACCESS_KEY=process.env.S3_SECRET_ACCESS_KEY|| 'S3_SECRET_ACCESS_KEY_TO_REPLACE'
 
 export default withAuth(
   config({
@@ -23,17 +23,11 @@ export default withAuth(
     },
     storage: {
       my_s3_files: {
-        // Files that use this store will be stored in an s3 bucket
         kind: 's3',
-        // This store is used for the file field type
         type: 'file',
-        // The S3 bucket name pulled from the S3_BUCKET_NAME environment variable above
         bucketName: S3_BUCKET_NAME,
-        // The S3 bucket region pulled from the S3_REGION environment variable above
         region: S3_REGION,
-        // The S3 Access Key ID pulled from the S3_ACCESS_KEY_ID environment variable above
         accessKeyId: S3_ACCESS_KEY_ID,
-        // The S3 Secret pulled from the S3_SECRET_ACCESS_KEY environment variable above
         secretAccessKey: S3_SECRET_ACCESS_KEY,
       },
     },
