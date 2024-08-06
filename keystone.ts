fe94ffcd5a9576c5525c26b665917592f09f2cdf
daniel-code20 <dgithub23@gmail.com>
2024-08-06 14:06:46 -0600

    upload

--- keystone.ts
+++ keystone.ts
@@ -3,6 +3,8 @@ import { config } from '@keystone-6/core';
 import { lists } from './schema';
 import { withAuth, session } from './auth';
 
+const baseUrl = 'https://libralite-production.up.railway.app/';
+
 export default withAuth(
   config({
     db: {
@@ -19,12 +21,13 @@ export default withAuth(
     storage: {
       my_s3_files: {
         kind: 's3',
-        type: 'file',
+        type: 'image',
         bucketName: process.env.S3_BUCKET_NAME || 'S3_BUCKET_NAME',
         region: process.env.S3_REGION || 'S3_REGION',
         accessKeyId: process.env.S3_ACCESS_KEY_ID || 'S3_ACCESS_KEY_ID',
         secretAccessKey: process.env.S3_SECRET_ACCESS_KEY || 'S3_SECRET_ACCESS_KEY',
         signed: { expiry: 3600 },
+        generateUrl: path => `${baseUrl}/${path}`,
       },
     },
   })
