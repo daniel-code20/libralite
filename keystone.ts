1f18e81d74bc7a7d5e48d2299f2ffc9667350c75
daniel-code20 <dgithub23@gmail.com>
2024-08-06 15:02:41 -0600

    upload

--- keystone.ts
+++ keystone.ts
@@ -3,8 +3,6 @@ import { config } from '@keystone-6/core';
 import { lists } from './schema';
 import { withAuth, session } from './auth';
 
-const baseUrl = 'https://libralite.s3.us-east-2.amazonaws.com';
-
 export default withAuth(
   config({
     db: {
@@ -26,8 +24,8 @@ export default withAuth(
         region: process.env.S3_REGION || 'S3_REGION',
         accessKeyId: process.env.S3_ACCESS_KEY_ID || 'S3_ACCESS_KEY_ID',
         secretAccessKey: process.env.S3_SECRET_ACCESS_KEY || 'S3_SECRET_ACCESS_KEY',
-        signed: { expiry: 3600 },
-        generateUrl: path => `${baseUrl}/${path}`,
+        signed: { expiry: 5000 },
+        endpoint: 'https://libralite.s3.us-east-2.amazonaws.com'
       },
     },
   })
