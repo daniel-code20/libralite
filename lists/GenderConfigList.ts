54c87eb7e40b12a10e38113d958f8799926e10a4
daniel-code20 <dgithub23@gmail.com>
2024-08-06 17:02:47 -0600

    upload

--- lists/GenderConfigList.ts
+++ lists/GenderConfigList.ts
@@ -1,12 +1,12 @@
 import { ListConfig } from "@keystone-6/core";
-import { text, relationship, image } from "@keystone-6/core/fields";
+import { text, relationship, file } from "@keystone-6/core/fields";
 import { allowAll } from "@keystone-6/core/access";
 
 export const GenderConfigList: ListConfig<any> = {
   fields: {
     name: text(),
     books: relationship({ ref: "Book.gender", many: true }),
-    image: image({ storage: 'my_s3_files' }),
+    image: file({ storage: 'my_s3_files' }),
   },
   access: allowAll,
 };
