ed7db7ab89c6d232f43fead9201ff3015e1001fc
Daniel Tejada <160541350+daniel-code20@users.noreply.github.com>
2024-08-06 08:30:14 -0600

    Update keystone.ts

--- keystone.ts
+++ keystone.ts
@@ -29,7 +29,7 @@ const {
   S3_ACCESS_KEY_ID: accessKeyId = 'keystone',
   S3_SECRET_ACCESS_KEY: secretAccessKey = 'keystone',
   // The base URL to serve assets from
-  ASSET_BASE_URL: baseUrl = 'https://libralite-production.up.railway.app/api/graphql',
+  ASSET_BASE_URL: baseUrl = process.env.BASE_URL,
 } = process.env;
 
 export default withAuth(
@@ -65,24 +65,6 @@ export default withAuth(
         // Set serverRoute to null if you don't want a route to be created in Keystone
         // serverRoute: null
         storagePath: 'public/images',
-      },
-      my_s3_files: {
-
-        endpoint: 'https://libralite-production.up.railway.app/',
-        // Files that use this store will be stored in an s3 bucket
-        kind: 's3',
-        // This store is used for the file field type
-        type: 'file',
-        // The S3 bucket name pulled from the S3_BUCKET_NAME environment variable above
-        bucketName,
-        // The S3 bucket region pulled from the S3_REGION environment variable above
-        region,
-        // The S3 Access Key ID pulled from the S3_ACCESS_KEY_ID environment variable above
-        accessKeyId,
-        // The S3 Secret pulled from the S3_SECRET_ACCESS_KEY environment variable above
-        secretAccessKey,
-        // The S3 links will be signed so they remain private
-        signed: { expiry: 5000 },
       },
     },
   })
