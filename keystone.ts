ee037c01fdb4c5e0012a5eb3a43759292ed41e18
daniel-code20 <dgithub23@gmail.com>
2024-08-06 14:51:03 -0600

    upload

--- keystone.ts
+++ keystone.ts
@@ -3,17 +3,7 @@ import { config } from '@keystone-6/core';
 import { lists } from './schema';
 import { withAuth, session } from './auth';
 
-const {
-  // The S3 Bucket Name used to store assets
-  S3_BUCKET_NAME: bucketName = 'keystone-test',
-  // The region of the S3 bucket
-  S3_REGION: region = 'ap-southeast-2',
-  // The Access Key ID and Secret that has read/write access to the S3 bucket
-  S3_ACCESS_KEY_ID: accessKeyId = 'keystone',
-  S3_SECRET_ACCESS_KEY: secretAccessKey = 'keystone',
-  // The base URL to serve assets from
-  ASSET_BASE_URL: baseUrl = 'http://localhost:3000',
-} = process.env;
+const baseUrl = 'https://libralite.s3.us-east-2.amazonaws.com';
 
 export default withAuth(
   config({
@@ -30,20 +20,14 @@ export default withAuth(
     },
     storage: {
       my_s3_files: {
-        // Files that use this store will be stored in an s3 bucket
         kind: 's3',
-        // This store is used for the file field type
-        type: 'file',
-        // The S3 bucket name pulled from the S3_BUCKET_NAME environment variable above
-        bucketName,
-        // The S3 bucket region pulled from the S3_REGION environment variable above
-        region,
-        // The S3 Access Key ID pulled from the S3_ACCESS_KEY_ID environment variable above
-        accessKeyId,
-        // The S3 Secret pulled from the S3_SECRET_ACCESS_KEY environment variable above
-        secretAccessKey,
-        // The S3 links will be signed so they remain private
-        signed: { expiry: 5000 },
+        type: 'image',
+        bucketName: process.env.S3_BUCKET_NAME || 'S3_BUCKET_NAME',
+        region: process.env.S3_REGION || 'S3_REGION',
+        accessKeyId: process.env.S3_ACCESS_KEY_ID || 'S3_ACCESS_KEY_ID',
+        secretAccessKey: process.env.S3_SECRET_ACCESS_KEY || 'S3_SECRET_ACCESS_KEY',
+        signed: { expiry: 3600 },
+        generateUrl: path => `${baseUrl}/${path}`,
       },
     },
   })
